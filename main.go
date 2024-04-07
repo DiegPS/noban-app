e111c216d6320fe726c60cede9dd7dd56ded818f
Diego Pahua Silvan <dpahuas@gmail.com>
2024-04-07 12:30:25 -0700

    add frameless

--- main.go
+++ main.go
@@ -18,9 +18,10 @@ func main() {
 
 	// Create application with options
 	err := wails.Run(&options.App{
-		Title:  "noban",
-		Width:  1024,
-		Height: 768,
+		Title:     "noban",
+		Width:     1024,
+		Height:    768,
+		Frameless: true,
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
