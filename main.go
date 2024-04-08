5d68fa59e5ede2b6c4a531551f595e82001983f1
haru <ggalactiko@galactiko.net>
2024-04-07 20:17:13 -0600

    Fixed transparency (Mac), better icon

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+	"github.com/wailsapp/wails/v2/pkg/options/mac"
 	"github.com/wailsapp/wails/v2/pkg/options/windows"
 )
 
@@ -31,8 +32,13 @@ func main() {
 			app,
 		},
 		Windows: &windows.Options{
+
 			WebviewIsTransparent: true,
 			WindowIsTranslucent:  true,
+		},
+		Mac: &mac.Options{
+			WebviewIsTransparent: true,
+			WindowIsTranslucent: true,
 		},
 	})
 
