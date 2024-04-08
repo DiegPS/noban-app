d843cf9fdf59e1bc7fd126b6c8fa70a4885536e1
Diego Pahua Silvan <dpahuas@gmail.com>
2024-04-07 18:25:33 -0700

    features trash

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,49 +1,150 @@
 import './style.css';
 import './app.css';
+import autoComplete from "@tarekraafat/autocomplete.js";
 
-import logo from './assets/images/logo-universal.png';
-import {Greet} from '../wailsjs/go/main/App';
+import {WindowMaximise, WindowMinimise, Quit} from '../wailsjs/runtime/runtime';
 
-// Setup the greet function
-window.greet = function () {
-    // Get name
-    let name = nameElement!.value;
+let proxyHost = "https://elqueseaerrrsda.zeabur.app/";
+let inputSearch = document.getElementById("to-do-input") as HTMLInputElement;
+let iframeElement = (document.querySelector("iframe#preview") as HTMLIFrameElement);
+let inputAutocomplete = document.getElementById("autoComplete") as HTMLInputElement;
+let placeholderIframe = document.getElementById("placeholder-iframe") as HTMLDivElement;
 
-    // Check if the input is empty
-    if (name === "") return;
+document.getElementById("maximise")!.addEventListener("click", () => {
+    WindowMaximise();
+});
 
-    // Call App.Greet(name)
-    try {
-        Greet(name)
-            .then((result) => {
-                // Update result with data back from App.Greet()
-                resultElement!.innerText = result;
-            })
-            .catch((err) => {
-                console.error(err);
-            });
-    } catch (err) {
-        console.error(err);
+const autoCompleteJS = new autoComplete({
+    placeHolder: "Search for Page...",
+    data: {
+        src: [
+            "Facebook",
+            "YouTube",
+            "Google",
+            "Gmail",
+            "Amazon",
+            "Noticias",
+            "Tiempo",
+            "Traductor",
+            "Wikipedia",
+            "Netflix",
+            "WhatsApp",
+            "Instagram",
+            "Twitter",
+            "LinkedIn",
+            "Reddit",
+            "Ebay",
+            "Booking",
+            "Airbnb",
+            "Pinterest",
+            "Apple",
+        ]
+    },
+    resultsList: {
+        element: (list:any, data:any) => {
+            if (!data.results.length) {
+                const message = document.createElement("div");
+                message.setAttribute("class", "no_result");
+                message.innerHTML = `<span>Found No Results for "${data.query}"</span>`;
+                list.prepend(message);
+            }
+        },
+        noResults: true,
+    },
+    resultItem: {
+        highlight: true
+    },
+    events: {
+        input: {
+            selection: (event:any) => {
+                const selection = event.detail.selection.value;
+                autoCompleteJS.input.value = selection;
+            }
+        }
     }
+});
+
+document.getElementById("close")!.addEventListener("click", () => {
+    Quit();
+});
+
+document.getElementById("minimise")!.addEventListener("click", () => {
+    WindowMinimise();
+});
+
+iframeElement.src = "";
+
+inputSearch.value = iframeElement.src.slice(proxyHost.length + "?proxy_url=".length);
+
+const siteDictionary = {
+    "google": "https://www.google.com",
+    "facebook": "https://www.facebook.com",
+    "wikipedia": "https://www.wikipedia.org",
+    "reddit": "https://www.reddit.com",
+    "amazon": "https://www.amazon.com",
+    "twitter": "https://www.twitter.com",
+    "instagram": "https://www.instagram.com",
+    "linkedin": "https://www.linkedin.com",
+    "youtube": "https://www.youtube.com"
 };
 
-document.querySelector('#app')!.innerHTML = `
-    <img id="logo" class="logo">
-      <div class="result" id="result">Please enter your name below 👇</div>
-      <div class="input-box" id="input">
-        <input class="input" id="name" type="text" autocomplete="off" />
-        <button class="btn" onclick="greet()">Greet</button>
-      </div>
-    </div>
-`;
-(document.getElementById('logo') as HTMLImageElement).src = logo;
+function normalizeURL(url) {
+    url = url.toLowerCase();
+    if (siteDictionary[url]) {
+        return siteDictionary[url];
+    }
 
-let nameElement = (document.getElementById("name") as HTMLInputElement);
-nameElement.focus();
-let resultElement = document.getElementById("result");
+    if (!url.startsWith("http://") || !url.startsWith("https://")) {
+        url = "http://" + url;
+    }
+    return url;
+
+}
+
+if(iframeElement.src == "") {
+    iframeElement.style.display = "none";
+}
+
+inputSearch.addEventListener("keyup", (event) => {
+    console.log(iframeElement.src);
+
+    if (event.key === "Enter") {
+        let searchValue = inputSearch.value;
+        if (!searchValue) {
+            iframeElement.style.display = "none"
+            placeholderIframe.style.display = "grid";
+        }
+        let normalizedURL = normalizeURL(searchValue);
+        iframeElement.src = proxyHost + "?proxy_url=" + encodeURIComponent(normalizedURL);
+    }
+}
+);
+
+inputAutocomplete.addEventListener("keyup", (event) => {
+    console.log(iframeElement.src);
+    if (event.key === "Enter") {
+        placeholderIframe.style.display = "none";
+        iframeElement.src = proxyHost + "?proxy_url=" + "https://www.google.com/search?q=" +encodeURIComponent(inputAutocomplete.value);
+        inputSearch.value = iframeElement.src.slice(proxyHost.length + "?proxy_url=".length);
+    }
+}
+);
+
+function resizeIframe() {
+    if (!iframeElement) {
+        return;
+    }
+    let viewportHeight = window.innerHeight;
+    iframeElement.style.height = viewportHeight - 40 + "px";
+}
+
+resizeIframe();
+window.addEventListener("resize", resizeIframe);
+
+
 
 declare global {
     interface Window {
-        greet: () => void;
+        greet: () => void;  
     }
 }
